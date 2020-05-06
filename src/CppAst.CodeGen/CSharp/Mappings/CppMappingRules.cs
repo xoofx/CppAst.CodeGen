b084a0746c918dda7848c8201bff557f1bf47e28
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-06 17:29:03 -0400

    remove Yoyo

--- src/CppAst.CodeGen/CSharp/Mappings/CppMappingRules.cs
+++ src/CppAst.CodeGen/CSharp/Mappings/CppMappingRules.cs
@@ -51,20 +51,4 @@ namespace CppAst.CodeGen.CSharp
             return GetEnumerator();
         }
     }
-
-    public static class Yoyo
-    {
-        public static void Tester()
-        {
-            var rules = new CppMappingRules()
-            {
-                e => e.Map("name").Private(),
-                e => e.Map("xxx").Name("test").Private(),
-                e => e.MapAll<CppEnumItem>().CppAction((converter, element) => {}),
-                e => e.MapMacroToConst("MACRO", "int"),
-                e => e.MapType("int", "SharpDX.Int4"),
-                e => e.MapArrayType("int", 4, "SharpDX.Int4"),
-            };
-        }
-    }
 }
