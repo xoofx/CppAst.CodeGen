22fb047de112b23572aef380be5637cdc913d12c
Alexandre Mutel <alexandre_mutel@live.com>
2021-02-11 22:17:34 +0100

    Add a method to get the name of the type

--- src/CppAst.CodeGen/CSharp/CSharpType.cs
+++ src/CppAst.CodeGen/CSharp/CSharpType.cs
@@ -3,11 +3,19 @@
 // See license.txt file in the project root for full license information.
 
 using CppAst.CodeGen.Common;
+using Zio.FileSystems;
 
 namespace CppAst.CodeGen.CSharp
 {
     public abstract class CSharpType : CSharpElement
     {
         public abstract void DumpReferenceTo(CodeWriter writer);
+        
+        public string GetName()
+        {
+            var writer = new CodeWriter(new CodeWriterOptions(new MemoryFileSystem(), CodeWriterMode.Simple));
+            DumpReferenceTo(writer);
+            return writer.CurrentWriter.ToString();
+        }
     }
 }
