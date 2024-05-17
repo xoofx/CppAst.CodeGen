af685c3c913e881fcb4af3902cca00a78ba1a3bc
Alexandre Mutel <alexandre_mutel@live.com>
2024-05-17 05:59:58 +0200

    Add generic type reference

---
+++ src/CppAst.CodeGen/CSharp/CSharpGenericTypeReference.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Licensed under the BSD-Clause 2 license.
+// See license.txt file in the project root for full license information.
+
+using System;
+using CppAst.CodeGen.Common;
+
+namespace CppAst.CodeGen.CSharp;
+
+public class CSharpGenericTypeReference : CSharpType
+{
+    public CSharpGenericTypeReference(string name, CSharpType[] typeArguments)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        TypeArguments = typeArguments ?? throw new ArgumentNullException(nameof(typeArguments));
+    }
+
+    public string Name { get; set; }
+
+    public CSharpType[] TypeArguments { get; set; }
+
+    /// <inheritdoc />
+    public override void DumpTo(CodeWriter writer)
+    {
+        writer.Write(Name);
+        writer.Write("<");
+        for (var i = 0; i < TypeArguments.Length; i++)
+        {
+            if (i > 0)
+            {
+                writer.Write(", ");
+            }
+
+            TypeArguments[i].DumpReferenceTo(writer);
+        }
+
+        writer.Write(">");
+    }
+
+    /// <inheritdoc />
+    public override void DumpReferenceTo(CodeWriter writer) => DumpTo(writer);
+}
